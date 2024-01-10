5a624bf1f00d5b588dfcf5779b0e9d33a04b3491
geranhuns <gera.nuncio@gmail.com>
2024-01-10 15:23:46 -0600

    print all tasks function

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ let tiempoField = document.getElementById("tiempoInput");
 
 //Identificar botón que hará la acción
 let agregarBtn = document.getElementById("btnAgregarTarea");
-
 //Asignar las variables a un objeto para poder usarlas
 agregarBtn.addEventListener("click", () => {
   let tarea = tareaField.value;
@@ -13,7 +12,8 @@ agregarBtn.addEventListener("click", () => {
   if (tarea && tiempo) {
     let saveTarea = { tarea, tiempo };
     saveTareaDB(saveTarea);
-  } else alert("Llénelo compa");
+  } else alert("Para agregar una tarea hay que llenar ambos campos");
+  printAllTasks();
 });
 
 //Subir tarea a la base de datos
@@ -36,10 +36,78 @@ const getAllTareas = async () => {
     "https://tareas-bbf13-default-rtdb.firebaseio.com/.json"
   );
   let data = await response.json();
+  console.log(data);
   return data;
 };
-//Imprimir las tareas de la base de datos en la lista
- const createTareaItem = (tareaObject) => {
-     let {tarea, tiempo,key} =
 
- }
+//funcion para borrar
+const deleteTask = async (taskKey) => {
+  let response = await fetch(
+    "https://tareas-bbf13-default-rtdb.firebaseio.com/.json",
+    {
+      method: "DELETE",
+    }
+  );
+  let data = await response.json();
+  return data;
+};
+
+const turnObjIntoArray = async () => {
+  let tareasObj = await getAllTareas();
+  let tareasArray = Object.keys(tareasObj).map((key) => ({
+    ...tareasObj[key],
+    key,
+  }));
+  console.log(tareasArray);
+  return tareasArray;
+};
+
+//Imprimir las tareas de la base de datos en la lista
+const createTareaItem = (singleTask) => {
+  let { tarea, tiempo, key } = singleTask;
+
+  let newLi = document.createElement("li");
+  newLi.classList.add("list-group-item");
+
+  let newLiText = document.createTextNode(tarea);
+
+  let timeSpan = document.createElement("span");
+  let timeSpanText = document.createTextNode(" minutos");
+  timeSpan.classList.add("fw-bold");
+  timeSpan.append(` ${tiempo}`, timeSpanText);
+
+  let buttonWrapper = document.createElement("div");
+
+  let addTaskBtn = document.createElement("button");
+  let btnText = document.createTextNode("+");
+  addTaskBtn.classList.add("btn", "btn-success");
+  addTaskBtn.append(btnText);
+
+  let deleteTaskBtn = document.createElement("button");
+  let deleteTaskBtnText = document.createTextNode("Delete");
+  deleteTaskBtn.classList.add("btn", "btn-danger");
+  deleteTaskBtn.append(deleteTaskBtnText);
+  deleteTaskBtn.addEventListener("click", async () => {
+    let data = await deleteTask(key);
+    printAllTasks();
+  });
+
+  buttonWrapper.append(addTaskBtn, deleteTaskBtn);
+  newLi.append(newLiText, timeSpan, buttonWrapper);
+
+  return newLi;
+};
+
+const printAllTasks = async () => {
+  let tareasUl = document.getElementById("tareasPendientesUl");
+  tareasUl.innerHTML = "";
+
+  let taskArray = await turnObjIntoArray();
+  console.log(taskArray);
+  taskArray.forEach((element) => {
+    let taskItem = createTareaItem(element);
+    tareasUl.append(taskItem);
+  });
+};
+
+printAllTasks();
